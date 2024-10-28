772d663f74fb02d83e95cc99df662f81ff1dca38
DanielKaldi <Kaldi@rptu.de>
2024-10-28 20:34:51 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<style>
+    main{
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+        justify-content: center;
+        font-size: xx-large;
+    }
+
+</style>
+
+<main>
+    <h1>Visual Analytics Project</h1>
+    <p>Not much to see here yet</p>
+</main>
