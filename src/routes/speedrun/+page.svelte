3e6ea0603341c07ad56e5b1f65e7192741debd9f
DanielKaldi <Kaldi@rptu.de>
2024-12-04 14:35:59 +0100

    added correct api calls

--- src/routes/speedrun/+page.svelte
+++ src/routes/speedrun/+page.svelte
@@ -5,6 +5,12 @@
 	import Piechart from '../../Components/Piechart.svelte';
 	import { base } from '$app/paths';
 
+	let pageSize = 200;
+
+	let selectedCategory = 'Category';
+	let categoryList = [];
+	let categoryIds = [];
+
 	let selectedGame = 'Game';
 	let gamesList = [
 		'Super Mario 64',
@@ -17,8 +23,8 @@
 		'Grand Theft Auto V'
 	];
 	let gamesIDs = [
-		'm1zkog01',
-		'pd0qogl1',
+		'o1y9wo6q',
+		'76r55vd8',
 		'pd0qw9w1',
 		'nd28z0ed',
 		'xkdk5gdm',
@@ -30,12 +36,23 @@
 	let isLoading = false;
 	let isProcessing = false;
 
-	async function fetchGameData(game) {
+	async function fetchGameData(givenUrl, getCategories = false, game, category) {
 		isLoading = true;
-		const id = gamesIDs[gamesList.indexOf(game)];
+		let gameId = gamesIDs[gamesList.indexOf(game)];
+		let categoryId = categoryIds[categoryList.indexOf(category)];
+
+		let url = `https://www.speedrun.com/api/v1/runs?game=${gameId}&category=${categoryId}&max=${pageSize}`;
+
+		if (getCategories) {
+			url = `https://www.speedrun.com/api/v1/games/${gameId}/categories`;
+		}
+
+		if (givenUrl != '') {
+			url = givenUrl;
+		}
 
 		try {
-			const response = await fetch(`https://www.speedrun.com/api/v1/runs?game=${id}&max=100`, {
+			const response = await fetch(url, {
 				method: 'GET',
 				headers: {
 					'Content-Type': 'application/json'
@@ -54,11 +71,37 @@
 		return gameData;
 	}
 
+	async function loadData(game, category) {
+		var safetyCounter = 0; //todelte later
+		var allLoaded = false;
+		var data = [];
+		var fetchRes = {};
+		var url = '';
+		while (!allLoaded) {
+			safetyCounter += 1;
+			if (safetyCounter > 100) {
+				return data;
+			}
+			fetchRes = await fetchGameData(url, false, game, category);
+
+			if (fetchRes == {} || fetchRes.pagination.size < pageSize) {
+				data = data.concat(fetchRes.data);
+				return data;
+			}
+			if (fetchRes.pagination.offset == 0) {
+				url = fetchRes.pagination.links[0].uri;
+			} else {
+				url = fetchRes.pagination.links[1].uri;
+			}
+			data = data.concat(fetchRes.data);
+		}
+	}
+
 	//cleans Data, propably needs to be adjusted
 	function cleanData(data) {
 		var cleanedData = [];
-		for (let i = 0; i < data.data.length; i++) {
-			cleanedData.push({ id: data.data[i]['id'], status: data.data[i]['status']['status'] });
+		for (let i = 0; i < data.length; i++) {
+			cleanedData.push({ id: data[i]['id'], status: data[i]['status']['status'] });
 		}
 		return cleanedData;
 	}
@@ -70,16 +113,42 @@
 		return data;
 	}
 
+	async function setCategories(game) {
+		let fetchData = await fetchGameData('', true, game);
+		fetchData.data.forEach((e) => {
+			categoryList.push(e.name);
+			categoryIds.push(e.id);
+		});
+	}
+
+	async function setCategory(category) {
+		if (category == selectedCategory) {
+			return;
+		}
+
+		isProcessing = true;
+
+		selectedCategory = category;
+
+		let game = selectedGame;
+		gameData = null;
+		gameData = await loadData(game, category);
+		gameData = cleanData(gameData);
+		gameData = replaceIDs(gameData);
+
+		isProcessing = false;
+	}
+
 	async function setGame(game) {
 		if (game == selectedGame) {
 			return;
 		}
 		isProcessing = true;
+		selectedCategory = 'Category';
+		categoryList = [];
+		categoryIds = [];
 		selectedGame = game;
-		gameData = null;
-		gameData = await fetchGameData(game);
-		gameData = cleanData(gameData);
-		gameData = replaceIDs(gameData);
+		await setCategories(game);
 
 		isProcessing = false;
 	}
@@ -89,44 +158,53 @@
 	<h1 style="margin: 20px;">Visual Analytics Project</h1>
 	<h2 style="margin: 10px;">Speedrun Data from Speedrun.com</h2>
 	<!--<a class="customButton" href={base + '/'}>Car Data</a>-->
-	<p style="font-size: 16px;">
-		Not all data is loaded yet. Only the first 100 entries of every Query
-	</p>
+	{#if isProcessing}
+		<p>Loading data for {selectedGame}...</p>
+	{/if}
+
 	<div class="container">
-		<Button
-			class="h-20 w-40 text-black"
-			style="background:#EEEEEE; font-size:20px; border-radius:10px"
-			>{selectedGame}<ChevronDownOutline class="ms-2 h-2 w-6 text-white dark:text-white" /></Button
-		>
-		<Dropdown>
-			{#each gamesList as game}
-				<DropdownItem
-					onclick={() => setGame(game)}
-					class="h-10 w-40"
-					style="background:#EEEEEE; font-size: 16px">{game}</DropdownItem
-				>
-			{/each}
-		</Dropdown>
+		<div class="flex flex-col gap-5">
+			<Button
+				class="h-20 w-40 text-black"
+				style="background:#EEEEEE; font-size:20px; border-radius:10px"
+				>{selectedGame}<ChevronDownOutline
+					class="ms-2 h-2 w-6 text-white dark:text-white"
+				/></Button
+			>
+			<Dropdown>
+				{#each gamesList as game}
+					<DropdownItem
+						onclick={() => setGame(game)}
+						class="h-10 w-40"
+						style="background:#EEEEEE; font-size: 16px">{game}</DropdownItem
+					>
+				{/each}
+			</Dropdown>
+
+			<Button
+				class="h-20 w-40 text-black"
+				style="background:#EEEEEE; font-size:20px; border-radius:10px"
+				>{selectedCategory}<ChevronDownOutline
+					class="ms-2 h-2 w-6 text-white dark:text-white"
+				/></Button
+			>
+			<Dropdown>
+				{#each categoryList as category}
+					<DropdownItem
+						onclick={() => setCategory(category)}
+						class="h-10 w-40"
+						style="background:#EEEEEE; font-size: 16px">{category}</DropdownItem
+					>
+				{/each}
+			</Dropdown>
+		</div>
 		{#if (gameData != null) & !isProcessing}
 			<Piechart width={200} height={200} labels="status" data={gameData} />
+			<!--
+				<Scatterplot width={400} heigth={400} keyX="date" keyY="times.primary_t" data={gameData} />
+			-->
 		{:else}
 			<div style="width:200px; height:500px; background-color:whitesmoke"></div>
-		{/if}
-	</div>
-
-	<div class="container" style="font-size: 16px;">
-		<!-- Display API Data -->
-		{#if isProcessing}
-			<p>Loading data for {selectedGame}...</p>
-		{:else if gameData}
-			{#if gameData.error}
-				<p>Error: {gameData.error}</p>
-			{:else}
-				<!-- Display game data -->
-				<pre>{JSON.stringify(gameData[0], null, 2)}</pre>
-			{/if}
-		{:else if selectedGame !== 'Select a Game'}
-			<p>No data loaded yet.</p>
 		{/if}
 	</div>
 </main>
